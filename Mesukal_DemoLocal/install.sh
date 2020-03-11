7979e1c08db4d208742f00d25caaa716f596ad50
Clément Dutriez <dutriezcle@gmail.com>
2020-03-11 17:31:38 +0100

    install.sh update

--- Mesukal_DemoLocal/install.sh
+++ Mesukal_DemoLocal/install.sh
@@ -1,7 +1,24 @@
 
+sudo apt update
+sudo apt upgrade
+
+
+
+sudo apt-get install tshark
+sudo apt-get install vlc
+sudo apt-get install wmctrl
+sudo apt install python3-pip
+sudo apt install python3-tk
+
+##### pip3 install
+pip3 install pathlib
+pip3 install pandas
+pip3 install matplotlib
+pip3 install psutil
+pip3 install tkinter
+pip3 install --pre scapy
+
 # update a youtube.luac
 cd /usr/lib/x86_64-linux-gnu/vlc/lua/playlist/
 sudo rm youtube*
 sudo wget https://raw.githubusercontent.com/videolan/vlc/master/share/lua/playlist/youtube.lua
-
-# 
